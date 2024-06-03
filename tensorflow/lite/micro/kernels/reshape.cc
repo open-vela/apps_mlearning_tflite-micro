b485340bdbcd750c021f83d1d7029750cf39e0ce
xinhaiteng <xinhaiteng@xiaomi.com>
2024-06-03 16:24:26 +0800

    Compile reshape
    Signed-off-by: xinhaiteng <xinhaiteng@xiaomi.com>

--- tensorflow/lite/micro/kernels/reshape.cc
+++ tensorflow/lite/micro/kernels/reshape.cc
@@ -49,11 +49,58 @@ TfLiteStatus EvalReshapeReference(TfLiteContext* context, TfLiteNode* node) {
   return kTfLiteOk;
 }
 
+#ifdef TFLITE_MODEL_COMPILER
+TfLiteStatus ComplileReshapeReference(TfLiteContext* context, TfLiteNode* node,
+                                      TfLiteCompileStep step, std::ofstream& ofs) {
+  switch (step) {
+    case kTfLiteCompileStepInclude:
+      ofs << "#include \"tensorflow/lite/micro/kernels/reshape.h\"" << std::endl
+          << std::endl;
+      break;
+
+    case kTfLiteCompileStepEval: {
+      const TfLiteEvalTensor* input =
+          tflite::micro::GetEvalInput(context, node, kReshapeInputTensor);
+      TfLiteEvalTensor* output =
+          tflite::micro::GetEvalOutput(context, node, kReshapeOutputTensor);
+
+      size_t input_bytes;
+      TF_LITE_ENSURE_STATUS(TfLiteTypeSizeOf(input->type, &input_bytes));
+      input_bytes *= ElementCount(*input->dims);
+
+      ofs << "{ // reshape" << std::endl;
+
+      if (input->data.raw != output->data.raw) {
+
+        micro::CompileAddress(ofs, "input", input->data.raw);
+        micro::CompileAddress(ofs, "output", output->data.raw);
+
+        ofs << "memcpy(output, input,"
+            << input_bytes
+            << ");" << std::endl;
+      }
+
+      ofs << "}" << std::endl;
+    } break;
+
+    default:
+      return kTfLiteError;
+  }
+
+  return kTfLiteOk;
+}
+#endif
+
 }  // namespace
 
 TFLMRegistration Register_RESHAPE() {
+#ifdef TFLITE_MODEL_COMPILER
+  return tflite::micro::CompileOp(nullptr, PrepareReshapeReference,
+                                  EvalReshapeReference, ComplileReshapeReference);
+#else
   return tflite::micro::RegisterOp(nullptr, PrepareReshapeReference,
-                                   EvalReshapeReference);
+                                   EvalReshapeReference, nullptr, nullptr);
+#endif
 }
 
 }  // namespace tflite
