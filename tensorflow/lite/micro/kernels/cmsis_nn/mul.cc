388c13a9f5e7a3e6f7e43302ec7b40c50717d8e1
xinhaiteng <xinhaiteng@xiaomi.com>
2024-06-27 15:27:03 +0800

    mul int8 compile func
    The compile function of the Mul operator is implemented, and the results of the two "if" branches are tested on the watch and are correct.
    
    Signed-off-by: xinhaiteng <xinhaiteng@xiaomi.com>

--- tensorflow/lite/micro/kernels/cmsis_nn/mul.cc
+++ tensorflow/lite/micro/kernels/cmsis_nn/mul.cc
@@ -151,6 +151,121 @@ TfLiteStatus EvalInt8(TfLiteContext* context, TfLiteNode* node) {
   return kTfLiteOk;
 }
 
+#ifdef TFLITE_MODEL_COMPILER
+TfLiteStatus CompileInt8(TfLiteContext* context, TfLiteNode* node,
+                         TfLiteCompileStep step, std::ofstream& ofs) {
+  switch (step) {
+    case kTfLiteCompileStepInclude:
+      ofs << "#include \"Include/arm_nnfunctions.h\""
+          << std::endl
+          << "#include \"tensorflow/lite/kernels/internal/reference/mul.h\""
+          << std::endl
+          << "#include \"tensorflow/lite/kernels/internal/reference/"
+             "integer_ops/mul.h\""
+          << std::endl;
+      break;
+
+    case kTfLiteCompileStepEval: {
+      TFLITE_DCHECK(node->builtin_data != nullptr);
+      TFLITE_DCHECK(node->user_data != nullptr);
+
+      const OpDataMul* data = static_cast<const OpDataMul*>(node->user_data);
+      const TfLiteEvalTensor* input1 =
+          tflite::micro::GetEvalInput(context, node, kMulInput1Tensor);
+      const TfLiteEvalTensor* input2 =
+          tflite::micro::GetEvalInput(context, node, kMulInput2Tensor);
+      TfLiteEvalTensor* output =
+          tflite::micro::GetEvalOutput(context, node, kMulOutputTensor);
+      TFLITE_DCHECK(input1->type == kTfLiteInt8);
+
+      tflite::ArithmeticParams op_params = {};
+
+      op_params.quantized_activation_min = data->output_activation_min;
+      op_params.quantized_activation_max = data->output_activation_max;
+      op_params.float_activation_max = data->output_activation_max_f32;
+      op_params.input1_offset = -data->input1_zero_point;
+      op_params.input2_offset = -data->input2_zero_point;
+      op_params.output_offset = data->output_zero_point;
+      op_params.output_multiplier = data->output_multiplier;
+      op_params.output_shift = data->output_shift;
+
+      bool need_broadcast = reference_ops::ProcessBroadcastShapes(
+          tflite::micro::GetTensorShape(input1),
+          tflite::micro::GetTensorShape(input2), &op_params);
+
+      ofs << "{ // mul int8" << std::endl;
+
+      tflite::micro::CompileAddress(
+          ofs, "input1", tflite::micro::GetTensorData<int8_t>(input1));
+      tflite::micro::CompileAddress(
+          ofs, "input2", tflite::micro::GetTensorData<int8_t>(input2));
+      tflite::micro::CompileAddress(
+          ofs, "output", tflite::micro::GetTensorData<int8_t>(output));
+
+      if (need_broadcast) {
+        ofs << "tflite::ArithmeticParams op_params = {"
+            << ".broadcast_category="
+            << reference_ops::BroadcastableOpCategoryToString(
+                  op_params.broadcast_category) << ", "
+            << ".input1_offset=" << op_params.input1_offset << ", "
+            << ".input2_offset=" << op_params.input2_offset << ", "
+            << ".output_offset=" << op_params.output_offset << ", "
+            << ".output_multiplier=" << op_params.output_multiplier << ", "
+            << ".output_shift=" << op_params.output_shift << ", "
+            << ".left_shift=" << op_params.left_shift << ", "
+            << ".input1_multiplier=" << op_params.input1_multiplier << ", "
+            << ".input1_shift=" << op_params.input1_shift << ", "
+            << ".input2_multiplier=" << op_params.input2_multiplier << ", "
+            << ".input2_shift=" << op_params.input2_shift << ", "
+            << ".quantized_activation_min="
+            << op_params.quantized_activation_min << ", "
+            << ".quantized_activation_max="
+            << op_params.quantized_activation_max << "}};"
+            << std::endl;
+
+        tflite::micro::CompileArray(ofs, "const int32_t", "input1_dims_data",
+                            input1->dims->data, input1->dims->size);
+        tflite::micro::CompileArray(ofs, "const int32_t", "input2_dims_data",
+                            input2->dims->data, input2->dims->size);
+        tflite::micro::CompileArray(ofs, "const int32_t", "output_dims_data",
+                                    output->dims->data, output->dims->size);
+
+        ofs << "tflite::reference_integer_ops::BroadcastMul4DSlow(op_params, "
+            << "tflite::RuntimeShape(" << input1->dims->size
+            << ", input1_dims_data), " << "input1, "
+            << "tflite::RuntimeShape(" << input2->dims->size
+            << ", input2_dims_data), " << "input2, "
+            << "tflite::RuntimeShape(" << output->dims->size
+            << ", output_dims_data), " << "output);"
+            << std::endl;
+      } else {
+        const auto shape1 = tflite::micro::GetTensorShape(input1);
+        const auto shape2 = tflite::micro::GetTensorShape(input2);
+        const auto shape3 = tflite::micro::GetTensorShape(output);
+        const int32_t block_size = MatchingElementsSize(shape1, shape2, shape3);
+
+        ofs << "arm_elementwise_mul_s8(input1, input2, "
+            << op_params.input1_offset << ", "
+            << op_params.input2_offset << ", output, "
+            << op_params.output_offset << ", "
+            << op_params.output_multiplier << ", "
+            << op_params.output_shift << ", "
+            << op_params.quantized_activation_min << ", "
+            << op_params.quantized_activation_max << ", "
+            << block_size << ");" << std::endl;
+      }
+      ofs << "}" << std::endl;
+
+    } break;
+
+    default:
+      return kTfLiteError;
+  }
+
+  return kTfLiteOk;
+}
+#endif
+
 TfLiteStatus EvalInt16(TfLiteContext* context, TfLiteNode* node) {
   TFLITE_DCHECK(node->builtin_data != nullptr);
   TFLITE_DCHECK(node->user_data != nullptr);
@@ -174,7 +289,11 @@ TFLMRegistration Register_MUL() {
 }
 
 TFLMRegistration Register_MUL_INT8() {
+#ifdef TFLITE_MODEL_COMPILER
+  return tflite::micro::CompileOp(MulInit, MulPrepare, EvalInt8, CompileInt8);
+#else
   return tflite::micro::RegisterOp(MulInit, MulPrepare, EvalInt8);
+#endif
 }
 
 TFLMRegistration Register_MUL_INT16() {
