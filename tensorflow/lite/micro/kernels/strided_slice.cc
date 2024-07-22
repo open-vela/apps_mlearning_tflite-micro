45bfd6b9c5c6a94dde65ffba7da84cc6ecbbb09c
xinhaiteng <xinhaiteng@xiaomi.com>
2024-07-22 21:17:21 +0800

    StridedSlice compile func
    Stridedslice operator compile function, and test passed.
    
    Signed-off-by: xinhaiteng <xinhaiteng@xiaomi.com>

--- tensorflow/lite/micro/kernels/strided_slice.cc
+++ tensorflow/lite/micro/kernels/strided_slice.cc
@@ -82,11 +82,116 @@ TfLiteStatus StridedSliceEval(TfLiteContext* context, TfLiteNode* node) {
   return kTfLiteOk;
 }
 
+#ifdef TFLITE_MODEL_COMPILER
+TfLiteStatus StridedSliceCompile(TfLiteContext* context, TfLiteNode* node,
+                                 TfLiteCompileStep step, std::ofstream& ofs) {
+
+  switch (step) {
+    case kTfLiteCompileStepInclude:
+      ofs << "#include \"tensorflow/lite/kernels/internal/reference/"
+          <<  "strided_slice.h\"" << std::endl;
+      break;
+
+    case kTfLiteCompileStepEval: {
+      TFLITE_DCHECK(node->user_data != nullptr);
+      const StridedSliceParams& op_params =
+          *(static_cast<const StridedSliceParams*>(node->user_data));
+
+      const TfLiteEvalTensor* input =
+          tflite::micro::GetEvalInput(context, node, kStridedSliceInputTensor);
+      TfLiteEvalTensor* output =
+          tflite::micro::GetEvalOutput(context, node, kStridedSliceOutputTensor);
+
+      ofs << "{ // transpose" << std::endl;
+
+      tflite::micro::CompileAddress(
+          ofs, "input", tflite::micro::GetTensorData<int8_t>(input));
+      tflite::micro::CompileAddress(
+          ofs, "output", tflite::micro::GetTensorData<int8_t>(output));
+
+      ofs << "const tflite::StridedSliceParams& op_params = {"
+          << ".start_indices_count="
+          << std::to_string(op_params.start_indices_count)
+          << ",.start_indices={";
+      for (int i = 0; i < 5; ++i)
+        ofs << op_params.start_indices[i] << ",";
+      ofs << "},.stop_indices_count="
+          << std::to_string(op_params.stop_indices_count)
+          << ",.stop_indices={";
+      for (int i = 0; i < 5; i++)
+        ofs << op_params.stop_indices[i] << ",";
+      ofs << "},.strides_count="
+          << std::to_string(op_params.strides_count)
+          << ",.strides={";
+      for (int i = 0; i < 5; i++)
+        ofs << op_params.strides[i] << ",";
+      ofs << "},.begin_mask=" << op_params.begin_mask
+          << ",.ellipsis_mask=" << op_params.ellipsis_mask
+          << ",.end_mask=" << op_params.end_mask
+          << ",.new_axis_mask=" << op_params.new_axis_mask
+          << ",.shrink_axis_mask=" << op_params.shrink_axis_mask
+          << ",.offset=" << op_params.offset
+          << "};" <<std::endl;
+
+      tflite::micro::CompileArray(ofs, "const int32_t", "input_dims_data",
+                          input->dims->data, input->dims->size);
+      tflite::micro::CompileArray(ofs, "const int32_t", "output_dims_data",
+                          output->dims->data, output->dims->size);
+
+      const char* type_str;
+      switch (output->type) {
+        case kTfLiteFloat32:
+          type_str = "float*";
+          break;
+        case kTfLiteInt8:
+          type_str = "int8_t*";
+          break;
+        case kTfLiteInt16:
+          type_str = "int16_t*";
+          break;
+        case kTfLiteInt32:
+          type_str = "int32_t*";
+          break;
+        case kTfLiteBool:
+          type_str = "bool*";
+          break;
+        default:
+          MicroPrintf("Type %s (%d) not supported.",
+                      TfLiteTypeGetName(input->type), input->type);
+          return kTfLiteError;
+      }
+
+      ofs << "tflite::reference_ops::StridedSlice(op_params, "
+          << "tflite::RuntimeShape(" << input->dims->size
+          << ", input_dims_data), "
+          << "reinterpret_cast<" << type_str << ">(input), "
+          << "tflite::RuntimeShape(" << output->dims->size
+          << ", output_dims_data), "
+          << "reinterpret_cast<" << type_str << ">(output));"
+          << std::endl;
+
+      ofs << "}" << std::endl;
+
+    } break;
+
+    default:
+      return kTfLiteError;
+  }
+
+  return kTfLiteOk;
+}
+#endif
+
 }  // namespace
 
 TFLMRegistration Register_STRIDED_SLICE() {
+#ifdef TFLITE_MODEL_COMPILER
+  return tflite::micro::CompileOp(StridedSliceInit, StridedSlicePrepare,
+                                   StridedSliceEval, StridedSliceCompile);
+#else
   return tflite::micro::RegisterOp(StridedSliceInit, StridedSlicePrepare,
                                    StridedSliceEval);
+#endif
 }
 
 }  // namespace tflite
