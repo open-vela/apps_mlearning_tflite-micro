3ac356992ec12503351244eb32e5a89aba9077f9
jihandong <jihandong@xiaomi.com>
2025-01-09 14:40:05 +0800

    Update beco fc, and fix some errors
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- tensorflow/lite/micro/kernels/beco/fully_connected.cc
+++ tensorflow/lite/micro/kernels/beco/fully_connected.cc
@@ -36,6 +36,7 @@ struct OpData {
   OpDataFullyConnected reference_op_data;
   int buffer_idx_filter;
   int buffer_idx_bias;
+  int buffer_idx_hw;
 };
 
 void* Init(TfLiteContext* context, const char* buffer, size_t length) {
@@ -77,8 +78,22 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
       filter->dims->data[0] * filter->dims->data[1],
       &data->buffer_idx_filter));
   TF_LITE_ENSURE_STATUS(micro_context->RequestScratchBufferInArena(
-      bias->dims->data[1] * sizeof(int32_t),
+      bias->dims->data[0] * sizeof(int32_t),
       &data->buffer_idx_bias));
+
+  cmsis_nn_dims output_dims;
+  output_dims.n = output->dims->data[0];
+  output_dims.h = 1;
+  output_dims.w = 1;
+  output_dims.c = output->dims->data[1];
+
+  auto buffer_size = beco_fully_connected_s8_get_buffer_size(&output_dims);
+  if (buffer_size > 0) {
+    TF_LITE_ENSURE_STATUS(micro_context->RequestScratchBufferInArena(
+      buffer_size, &data->buffer_idx_hw));
+  } else {
+    data->buffer_idx_hw = -1;
+  }
 
   // Initialize fc reference parameters.
   TF_LITE_ENSURE_STATUS(CalculateOpDataFullyConnected(
@@ -107,6 +122,7 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
 
   TFLITE_DCHECK(node->user_data != nullptr);
   const OpData& data = *(static_cast<const OpData*>(node->user_data));
+  MicroContext* micro_context = GetMicroContext(context);
 
   // Initialize beco fc parameters.
   cmsis_nn_fc_params fc_params;
@@ -117,14 +133,14 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   fc_params.activation.max = data.reference_op_data.output_activation_max;
 
   cmsis_nn_per_tensor_quant_params quant_params;
-  quant_params.multiplier = data.output_multiplier;
-  quant_params.shift = data.output_shift;
+  quant_params.multiplier = data.reference_op_data.output_multiplier;
+  quant_params.shift = data.reference_op_data.output_shift;
 
   // Fetch buffers.
   auto filter_buffer = reinterpret_cast<int8_t*>(
-      micro_context->GetScratchBuffer(data->buffer_idx_filter));
+      micro_context->GetScratchBuffer(data.buffer_idx_filter));
   auto bias_buffer = reinterpret_cast<int32_t*>(
-      micro_context->GetScratchBuffer(data->buffer_idx_bias));
+      micro_context->GetScratchBuffer(data.buffer_idx_bias));
 
   // XXX: should consider other dims.
   cmsis_nn_dims input_dims;
@@ -147,10 +163,10 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   bias_dims.h = 1;
   bias_dims.w = 1;
   bias_dims.c = output->dims->data[1];
-  if (conv_params.input_offset != 0) {
+  if (fc_params.input_offset != 0) {
     beco::addOffset2Bias<int32_t,int8_t>(
       bias_buffer, tflite::micro::GetOptionalTensorData<int32_t>(bias),
-      conv_params.input_offset, filter_buffer, 0,
+      fc_params.input_offset, filter_buffer, 0,
       filter->dims->data[0], 1, 1, filter->dims->data[1]);
   } else {
     std::memcpy(bias_buffer, tflite::micro::GetOptionalTensorData<int32_t>(bias),
@@ -163,9 +179,17 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   output_dims.w = 1;
   output_dims.c = output->dims->data[1];
 
+  cmsis_nn_context ctx;
+  ctx.size = 0; // NOTE: not used in BECO
+  if (data.buffer_idx_hw > 0) {
+    ctx.buf = micro_context->GetScratchBuffer(data.buffer_idx_hw);
+  } else {
+    ctx.buf = nullptr;
+  }
+
   // Do the real job.
   BECO_INIT();
-  beco_fully_connected_s8(NULL, fc_params, quant_params,
+  beco_fully_connected_s8(&ctx, &fc_params, &quant_params,
     &input_dims,  tflite::micro::GetTensorData<int8_t>(input),
     &filter_dims, filter_buffer,
     &bias_dims,   bias_buffer,
@@ -177,6 +201,15 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
 
 }   // namespace
 
+#ifdef TFLITE_MODEL_COMPILER
+TFLMRegistration Register_BECO_FULLY_CONNECTED() {
+  return tflite::micro::RegisterOp(Init, Prepare, Eval);
+}
+
+TFLMRegistration Register_BECO_FULLY_CONNECTED_INT8() {
+  return tflite::micro::RegisterOp(Init, Prepare, Eval);
+}
+#else
 TFLMRegistration Register_FULLY_CONNECTED() {
   return tflite::micro::RegisterOp(Init, Prepare, Eval);
 }
@@ -184,5 +217,6 @@ TFLMRegistration Register_FULLY_CONNECTED() {
 TFLMRegistration Register_FULLY_CONNECTED_INT8() {
   return tflite::micro::RegisterOp(Init, Prepare, Eval);
 }
+#endif
 
 }   // namespace tflite
