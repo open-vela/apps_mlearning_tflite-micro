b4b89cd7794038b911cb9630084fe89ca0446194
jihandong <jihandong@xiaomi.com>
2025-01-26 16:18:27 +0800

    Support compiling pure-software softmax int8.
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- tensorflow/lite/micro/kernels/softmax.cc
+++ tensorflow/lite/micro/kernels/softmax.cc
@@ -81,10 +81,80 @@ TfLiteStatus SoftmaxEval(TfLiteContext* context, TfLiteNode* node) {
       return kTfLiteError;
   }
 }
+
+#ifdef TFLITE_MODEL_COMPILER
+TfLiteStatus SoftmaxCompile(TfLiteContext* context, TfLiteNode* node,
+                            TfLiteCompileStep step, std::ofstream& ofs) {
+  switch (step) {
+    case kTfLiteCompileStepInclude:
+      ofs << "#include \"tensorflow/lite/kernels/internal/reference/softmax.h\""
+          << std::endl
+          << "#include \"tensorflow/lite/kernels/internal/types.h\""
+          << std::endl;
+      break;
+
+    case kTfLiteCompileStepEval: {
+      const TfLiteEvalTensor* input = tflite::micro::GetEvalInput(context, node, 0);
+      TfLiteEvalTensor* output = tflite::micro::GetEvalOutput(context, node, 0);
+
+      TFLITE_DCHECK(node->user_data != nullptr);
+      SoftmaxParams op_data = *static_cast<SoftmaxParams*>(node->user_data);
+
+      ofs << "{ // softmax" << std::endl;
+
+      tflite::micro::CompileAddress(ofs, "input_data", input->data.data);
+      tflite::micro::CompileAddress(ofs, "output_data", output->data.data);
+      tflite::micro::CompileArray(ofs, "const int32_t", "input_dims_data",
+                                  input->dims->data, input->dims->size);
+      tflite::micro::CompileArray(ofs, "const int32_t", "output_dims_data",
+                                  output->dims->data, output->dims->size);
+
+      ofs << "tflite::SoftmaxParams params;"
+          << "params.beta=" << op_data.beta << ";"
+          << "params.input_multiplier=" << op_data.input_multiplier << ";"
+          << "params.input_left_shift=" << op_data.input_left_shift << ";"
+          << "params.reverse_scaling_divisor=" << op_data.reverse_scaling_divisor << ";"
+          << "params.reverse_scaling_right_shift=" << op_data.reverse_scaling_right_shift << ";"
+          << "params.diff_min=" << op_data.diff_min << ";"
+          << "params.zero_point=" << op_data.zero_point << ";"
+          << "params.scale=" << op_data.scale << ";"
+          << std::endl;
+
+      switch(input->type) {
+        case kTfLiteInt8:
+          ofs << "tflite::reference_ops::Softmax(params,"
+              << "tflite::RuntimeShape("<< input->dims->size << ",input_dims_data),"
+              << "(int8_t*)input_data,"
+              << "tflite::RuntimeShape("<< output->dims->size << ",output_dims_data),"
+              << "(int8_t*)output_data);"
+              << std::endl;
+          break;
+        default:
+          ofs << "Input type " << TfLiteTypeGetName(input->type)
+              << " not supported." << std::endl;
+          return kTfLiteError;
+      }
+
+      ofs << "}" << std::endl;
+      break;
+    }
+
+    default:
+      return kTfLiteError;
+  }
+
+  return kTfLiteOk;
+}
+#endif
 }  // namespace
 
 TFLMRegistration Register_SOFTMAX() {
+#ifdef TFLITE_MODEL_COMPILER
+  return tflite::micro::CompileOp(SoftmaxInit, SoftmaxPrepare, SoftmaxEval,
+                                  SoftmaxCompile);
+#else
   return tflite::micro::RegisterOp(SoftmaxInit, SoftmaxPrepare, SoftmaxEval);
+#endif
 }
 
 }  // namespace tflite
